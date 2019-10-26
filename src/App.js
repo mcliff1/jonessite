e6e90204fec70527ec96b7c99874691782cd6340
Matt Cliff <matt@denvercliffs.com>
2019-10-26 17:08:23 -0600

    added navigation block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './components/About';
 import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <About />
       <Contact />
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <p>This is where we talk about our self.
         Show our stuff.
         Why we be so cool.
         </p>
         <p>maybe we put a green border or background</p>
+        <aaabbr>was ist los</aaabbr>
+
       </div>
     );
   }
