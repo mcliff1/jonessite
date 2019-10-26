a73c2b2fc5579d712cd9d61e212d0a42bd5d18b6
Matt Cliff <matt@denvercliffs.com>
2019-10-26 17:10:07 -0600

    adding some bootstrap components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="container bg-light">
         <p>
         In this section we want to write something about how to contact us.
         Maybe some email icon, or twitter bird.
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return (
+      <nav class="navbar navbar-expand-lg bg-light">
       <div>Navigation</div>
+      </nav>
     )
   }
 }
