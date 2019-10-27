17e4a61e850cc657aca4ed41eb1576d1754e3ee0
Matt Cliff <matt@denvercliffs.com>
2019-10-26 19:42:54 -0600

    using bootstrap

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg bg-light">
-      <div>Navigation</div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <a className="navbar-brand" href="/">Navigation</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" area-expanded="false" aria-label="Toggle Nav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li>ok</li>
+          </ul>
+        </div>
+
       </nav>
     )
   }
