0dcde6978980e7728f746e0f90a26236386ab832
Matt Cliff <matt@denvercliffs.com>
2019-10-26 19:55:03 -0600

    add some basic bootstrap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,13 @@ class About extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>This is where we talk about our self.
-        Show our stuff.
-        Why we be so cool.
-        </p>
-        <p>maybe we put a green border or background</p>
-        <aaabbr>was ist los</aaabbr>
+        <div className="border border-primary rounded">
+          <p>This is where we talk about our self.
+          Show our stuff.
+          Why we be so cool.
+          </p>
+          <p>maybe we put a green border or background</p>
+        </div>
 
       </div>
     );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,12 @@ class Contact extends Component {
   render() {
     return (
       <div className="container bg-light">
-        <p>
-        In this section we want to write something about how to contact us.
-        Maybe some email icon, or twitter bird.
-      </p>
+        <div className="border border-info rounded">
+          <p>
+          In this section we want to write something about how to contact us.
+          Maybe some email icon, or twitter bird.
+          </p>
+        </div>
       </div>
     );
   }
