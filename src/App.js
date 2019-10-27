72300540561035119cc28af2e2899be134767b75
Matt Cliff <matt@denvercliffs.com>
2019-10-26 19:44:45 -0600

    wrapping in App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import About from './components/About';
 import Contact from './components/Contact';
 import Navigation from './components/Navigation';
-import './App.css';
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Navigation />
-      <About />
-      <Contact />
+      <div className="container">
+        <About />
+        <Contact />
+      </div>
     </div>
   );
 }
