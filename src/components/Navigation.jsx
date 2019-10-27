2485ae4d24151be2d7d27afa74fa6cefed1a65df
Matt Cliff <matt@denvercliffs.com>
2019-10-26 19:48:17 -0600

    adding a little to nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="/">Navigation</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" area-expanded="false" aria-label="Toggle Nav">
           <span className="navbar-toggler-icon"></span>
@@ -11,7 +11,12 @@ class Navigation extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li>ok</li>
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Link</a>
+            </li>
           </ul>
         </div>
 
