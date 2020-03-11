494ce129fb3ed50a8b5decc092adc7d651bc06fa
Matt Cliff <matt@denvercliffs.com>
2020-03-11 16:59:26 -0600

    added backkground image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
 //import Navigation from './components/Navigation';
 
+
+//import desktopImage from './background.png';
+// imageUrl = window.innerWidth > 649 ? desktopImage ? mobileImage
+
 function App() {
   return (
-    <div>
+    //<div style={{backgroundImage: `(${desktopImage})`}}>
+    <div className="bg">
       <div className="container">
         <About />
         <p></p>
