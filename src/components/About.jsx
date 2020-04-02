9996a5caacf436859808916f7972e3fc5dd6733e
Matt Cliff <matt@denvercliffs.com>
2020-04-01 21:15:20 -0600

    updates on layout and images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import InlineImage from '../images/redtruck_small.jpeg';
 
 class About extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="border border-primary rounded text-primary">
-          <h2>About Us</h2>
+      <Container fluid>
+        <h2>About Us</h2>
+        <Row>
+        <div className="border border-primary rounded">
           <p>Jones Logistics Corp is an agency owned by Chad Hubble operating under Priority 1 Inc</p>
           <p>Chad has over 26 years of experience handling truckload, LTL, air freight and small parcel</p>
           <p>Special Projects include moving of Flying Dog Brewery from Colorado to Maryland, high rise construction project delivery throughout the United States, heavy haul equipment moves to Fairbanks, Alaska</p>
           <p>Our combination of being with a well leveraged company and high level technology built by Priority 1 Inc making it easy to do business</p>
         </div>
+      </Row>
+      <Row>
+      <Col>
         <div className="border border-primary rounted">Project freight experience - Chad oversaw, priced and handled the entire Flying Dog Brewery move from Colorado to Maryland.  We worked with Flying Dog management to price up several potential site locations strategically looking at sales numbers and what made sense from a logistics perspective.  We contracted crane services for the pickup and delivery and arranged 11 flatbed shipments that included 3 oversized shipments requiring travel escorts and permitting.   All went well and the brewery was back up and running two weeks later in the process.  Our experience in handling such specialized freight created a savings in down time and was cost effective in coordination with the costly crane services that we were able to minimize through our preparedness.
         </div>
         <div className="border border-primary rounted">Warehouse partner services  - When RES - Renewable Energy Systems was installing a project in southern California, everything was going well until the government services created a delay with permitting of the final install.  The 40 ocean containers were already set to land in Long Beach and the demurrage cost was going to be in the thousands of dollars, RES came to Chad asking for a solution.  We established a partnership for drayage and warehouse storage of the freight and do a full inventory receiving of the 400+ solar panels for future delivery later in the summer.  Our solution again saved the client money and established a streamline of the deliveries when the facility was ready to proceed.
         </div>
+      </Col>
+      <Col xs><img src={InlineImage}></img></Col>
+      </Row>
 
-      </div>
+      </Container>
     );
   }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import InlineImage from '../images/TruckMountain_small.jpg';
 
 class Contact extends Component {
   render() {
     return (
-      <div className="container-fluid">
+      <Container fluid>
+        <h2>Contact Us</h2>
+        <Row>
+          <Col>
         <div className="border border-info rounded">
-          <h2>Let’s discuss your transportation needs today!</h2>
+          <b>Let’s discuss your transportation needs today!</b>
           <p>Please call or email Chad direct:</p>
   <p><a href="mailto:chubble@priority1inc.net" target="_blank" rel="noopener noreferrer" class="btn btn-primary">chubble@priority1inc.net</a></p>
   <p><a href="mailto:chad.hubble@gmail.com" target="_blank" rel="noopener noreferrer" class="btn btn-secondary">chad.hubble@gmail.com</a></p>
   <p><a href="tel:3036679595" class="phone">303-667-9595 direct</a></p>
         </div>
-      </div>
+         </Col>
+        <Col><img src={InlineImage}></img></Col>
+        </Row>
+      </Container>
     );
   }
 
