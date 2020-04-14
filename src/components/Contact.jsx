4775dab2b9bd576a538d5ab3c4b313f604bbfefb
Matt Cliff <matt@denvercliffs.com>
2020-04-13 21:25:14 -0600

    remove react-strap (use plain bootstrap)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
 import InlineImage from '../images/TruckMountain_small.jpg';
 
 class Contact extends Component {
   render() {
     return (
-      <Container fluid>
+      <div  className="container fluid">
         <h2>Contact Us</h2>
-        <Row>
-          <Col>
+        <div className="row">
+        <div className="col">
         <div className="border border-info rounded">
           <b>Let’s discuss your transportation needs today!</b>
           <p>Please call or email Chad direct:</p>
@@ -16,10 +15,10 @@ class Contact extends Component {
   <p><a href="mailto:chad.hubble@gmail.com" target="_blank" rel="noopener noreferrer" class="btn btn-secondary">chad.hubble@gmail.com</a></p>
   <p><a href="tel:3036679595" class="phone">303-667-9595 direct</a></p>
         </div>
-         </Col>
-        <Col><img src={InlineImage} alt="Trucking Logistics"></img></Col>
-        </Row>
-      </Container>
+      </div>
+        <div className="col"><img src={InlineImage} alt="Trucking Logistics"></img></div>
+      </div>
+    </div>
     );
   }
 
